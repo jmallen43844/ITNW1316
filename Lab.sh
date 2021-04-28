7923ffed6a7a061a2c774ed87d6c7db8d65bbc3e
Joshua Allen <josh2014allen@gmail.com>
2021-04-28 09:43:01 -0500

    revised for final

--- Lab.sh
+++ Lab.sh
@@ -1,1 +1,14 @@
+#!bin/bash
+echo -n "User: "
+whoami
+echo -n "@"
+hostname
+echo -n "CPU Info:  "
+lscpu | grep "Model name:"
+lscpu | grep "MHz"
+echo -n "Memory Info:  "
+free -ht
+df -Th | grep 'ext4\|xfs'
+echo -n "IP Address:  "
+ip addr | grep "/25"
 
